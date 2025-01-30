7cf3f7cfdb906b9a9d5fb24d1d9d75ea8c82656e
mukeshmahato17 <mukeshmahato1089@gmail.com>
2025-01-30 18:15:12 +0545

    .

--- types.go
+++ types.go
@@ -1,1 +1,21 @@
 package main
+
+import "math/rand/v2"
+
+type Account struct {
+	ID        int
+	FirstName string
+	LastName  string
+	Number    int64
+	Balance   int64
+}
+
+func NewAccount(firstName, lastName string) *Account {
+	return &Account{
+		ID:        rand.IntN(10),
+		FirstName: firstName,
+		LastName:  lastName,
+		Number:    int64(rand.IntN(10000)),
+		Balance:   int64(rand.IntN(10000000)),
+	}
+}
